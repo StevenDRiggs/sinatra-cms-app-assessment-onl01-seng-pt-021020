d156c9974c908f6a133066d3aed046af4e8ea615
StevenDRiggs <sfriggs21@gmail.com>
2020-05-19 03:38:46 -0600

    basic sources/new page

--- sth/app/controllers/harvests_controller.rb
+++ sth/app/controllers/harvests_controller.rb
@@ -1,24 +1,30 @@
 class HarvestsController < ApplicationController
 
-  get "/harvests" do
+  get '/harvests' do
     @harvests = Harvest.all
 
-    erb :"/harvests/index.html"
+    erb :'harvests/index.html'
   end
 
-  get "/harvests/:id" do
+  get '/harvests/:id' do
     @harvest = Harvest.find_by_id(params[:id])
 
-    erb :"/harvests/show.html"
+    erb :'harvests/show.html'
+  end
+  
+  get '/sources/new' do
+    @received_items = ReceivedItem.all
+
+    erb :'harvests/sources/new.html'
   end
 
-  get "/sources/:id" do
+  get '/sources/:id' do
     @source = Source.find_by_id(params[:id])
     @bible_references = @source.harvests.collect {|harvest| harvest.bible_references}.flatten.uniq
     @desires = @bible_references.collect {|bible_reference| bible_reference.desires}.flatten.uniq
     @targets = @bible_references.collect {|bible_reference| bible_reference.seeds}.flatten.uniq.collect {|seed| seed.targets}.flatten.uniq
 
-    erb :"/harvests/sources/show.html"
+    erb :'harvests/sources/show.html'
   end
 
 end
