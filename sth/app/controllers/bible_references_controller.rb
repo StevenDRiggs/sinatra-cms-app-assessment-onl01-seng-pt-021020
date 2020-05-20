3ed640d386f461b42d83f4d95626ecf464f55bd7
StevenDRiggs <sfriggs21@gmail.com>
2020-05-19 18:20:30 -0600

    post bible_references/new

--- sth/app/controllers/bible_references_controller.rb
+++ sth/app/controllers/bible_references_controller.rb
@@ -15,6 +15,34 @@ class BibleReferencesController < ApplicationController
     erb :"bible_references/new.html"
   end
 
+  post '/bible_references' do
+    if params[:reference] != ''
+      bible_reference = BibleReference.new(reference: params[:reference], content: params[:content])
+
+      if !(params[:desires].length == 1 && params[:desires][0] == '')
+        for desire_id in params[:desires]
+          bible_reference.desires << Desire.find_by_id(desire_id)
+        end
+      end
+
+      if !(params[:seeds].length == 1 && params[:seeds][0] == '')
+        for seed_id in params[:seeds]
+          bible_reference.seeds << Seed.find_by_id(seed_id)
+        end
+      end
+
+      if !(params[:harvests].length == 1 && params[:harvests][0] == '')
+        for harvest_id in params[:harvests]
+          bible_reference.harvests << Harvest.find_by_id(harvest_id)
+        end
+      end
+
+      bible_reference.save
+    end
+
+    redirect '/bible_references'
+  end
+
   get "/bible_references/:id" do
     @bible_reference = BibleReference.find_by_id(params[:id])
 
