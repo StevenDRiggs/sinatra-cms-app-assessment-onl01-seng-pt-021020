a027d10fdddadfc0aec8e051b5dfb80a467121d1
StevenDRiggs <sfriggs21@gmail.com>
2020-05-22 05:00:08 -0600

    added edit functionality to sources

--- sth/app/controllers/sources_controller.rb
+++ sth/app/controllers/sources_controller.rb
@@ -53,11 +53,35 @@ class SourcesController < ApplicationController
     erb :'/sources/show.html'
   end
 
+  # edit
   get '/sources/:id/edit' do
     @source = Source.find_by_id(params[:id])
     @received_items = ReceivedItem.all
 
     erb :'/sources/edit.html'
+  end
+
+  patch '/sources/:id' do
+    source = Source.find_by_id(params[:id])
+    source.name = params[:name]
+    source.time_received = "#{params[:months_received]} months, #{params[:weeks_received]} weeks, #{params[:days_received]} days, #{params[:hours_received]} hours, #{params[:minutes_received]} minutes"
+    source.money_received = params[:money_received]
+
+    source.received_items = []
+    if !(params[:received_items].nil? || (params[:received_items].length == 1 && params[:received_items][0] == ''))
+      for received_item_id in params[:received_items]
+        received_item = ReceivedItem.find_by_id(received_item_id)
+        source.received_items << received_item if received_item
+      end
+    end
+
+    if !(params[:new_received_item].nil? || params[:new_received_item] == '')
+      source.received_items.build(item: params[:new_received_item])
+    end
+
+    source.save
+
+    redirect "/sources/#{source.id}"
   end
 
 end
