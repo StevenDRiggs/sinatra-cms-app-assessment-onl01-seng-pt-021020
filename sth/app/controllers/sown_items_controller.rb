2265c071261d525ffe6c4074ccca11b26496d3af
StevenDRiggs <sfriggs21@gmail.com>
2020-05-22 07:03:19 -0600

    added new functionality to sown_items; corrected typo in sown_items/index

--- sth/app/controllers/sown_items_controller.rb
+++ sth/app/controllers/sown_items_controller.rb
@@ -6,5 +6,18 @@ class SownItemsController < ApplicationController
 
     erb :'/sown_items/index.html'
   end
-  
+
+  # new
+  get '/sown_items/new' do
+    @targets = Target.all
+
+    erb :'/sown_items/new.html'
+  end
+
+  post '/sown_items' do
+    sown_item = SownItem.create(item: params[:item], target: Target.find_by_id(params[:target]))
+
+    redirect "/sown_items"
+  end
+
 end
