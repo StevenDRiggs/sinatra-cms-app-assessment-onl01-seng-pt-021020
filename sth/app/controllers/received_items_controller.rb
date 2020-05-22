a4faa23b37e0969785720643bc19395c2d23889b
Steven Riggs <sfriggs21@gmail.com>
2020-05-21 21:46:57 -0600

    added new functionality to received_items

--- sth/app/controllers/received_items_controller.rb
+++ sth/app/controllers/received_items_controller.rb
@@ -7,4 +7,20 @@ class ReceivedItemsController < ApplicationController
     erb :'/received_items/index.html'
   end
 
+  # new
+  get '/received_items/new' do
+    @sources = Source.all
+
+    erb :'/received_items/new.html'
+  end
+
+  post '/received_items' do
+    received_item = ReceivedItem.create(item: params[:item], source: Source.find_by_id(params[:source]))
+
+    redirect "/received_items/#{received_item.id}"
+  end
+
+  # show
+  get '/received
+
 end
