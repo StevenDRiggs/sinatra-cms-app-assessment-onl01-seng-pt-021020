b6e36fa77a38aa86a3683228f684a6e45bee3bbb
Steven Riggs <sfriggs21@gmail.com>
2020-05-21 21:15:01 -0600

    added index functionality to received_items

--- sth/app/controllers/received_items_controller.rb
+++ sth/app/controllers/received_items_controller.rb
@@ -1,3 +1,10 @@
 class ReceivedItemsController < ApplicationController
 
+  # index
+  get '/received_items' do
+    @received_items = ReceivedItem.all
+
+    erb :'/received_items/index.html'
+  end
+
 end
