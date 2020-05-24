ce72a9970b644280c9d229ee180c8f5641d3339b
StevenDRiggs <sfriggs21@gmail.com>
2020-05-24 17:36:22 -0600

    refactored targets/edit for private user data

--- sth/app/controllers/targets_controller.rb
+++ sth/app/controllers/targets_controller.rb
@@ -81,57 +81,57 @@ class TargetsController < ApplicationController
       redirect '/'
     end
 
-    @target = Target.find_by_id(params[:id])
-    @items = (SownItem.all + ToSowItem.all).uniq
+    user = User.find_by_id(session[:rd])
+    @target = Target.find_by(user_id: user.id, id: params[:id])
+    @items = user.sown_items + user.to_sow_items
 
     erb :'/targets/edit.html'
   end
 
   patch '/targets/:id' do
-    if params[:name] != ''
-      target = Target.find_by_id(params[:id])
-      target.name = params[:name]
-      target.time_sown = "#{params[:months_sown]} months, #{params[:weeks_sown]} weeks, #{params[:days_sown]} days, #{params[:hours_sown]} hours, #{params[:minutes_sown]} minutes"
-      target.money_sown = params[:money_sown].to_f
-      target.time_to_sow = "#{params[:months_to_sow]} months, #{params[:weeks_to_sow]} weeks, #{params[:days_to_sow]} days, #{params[:hours_to_sow]} hours, #{params[:minutes_to_sow]} minutes"
-      target.money_to_sow = params[:money_to_sow].to_f
+    user = User.find_by_id(session[:rd])
+    target = Target.find_by(user_id: user.id, id:params[:id])
+    target.name = params[:name]
+    target.time_sown = "#{params[:months_sown]} months, #{params[:weeks_sown]} weeks, #{params[:days_sown]} days, #{params[:hours_sown]} hours, #{params[:minutes_sown]} minutes"
+    target.money_sown = params[:money_sown].to_f
+    target.time_to_sow = "#{params[:months_to_sow]} months, #{params[:weeks_to_sow]} weeks, #{params[:days_to_sow]} days, #{params[:hours_to_sow]} hours, #{params[:minutes_to_sow]} minutes"
+    target.money_to_sow = params[:money_to_sow].to_f
 
-      target.sown_items = []
-      target.to_sow_items = []
+    target.sown_items = []
+    target.to_sow_items = []
 
-      target.sown_items << SownItem.create(item: params[:new_sown_item]) if !(params[:new_sown_item].nil? || params[:new_sown_item] == '')
-      target.to_sow_items << ToSowItem.create(item: params[:new_to_sow_item]) if !(params[:new_to_sow_item].nil? || params[:new_to_sow_item] != '')
+    target.sown_items << user.sown_items.build(item: params[:new_sown_item]) if !(params[:new_sown_item].nil? || params[:new_sown_item] == '')
+    target.to_sow_items << user.to_sow_items.build(item: params[:new_to_sow_item]) if !(params[:new_to_sow_item].nil? || params[:new_to_sow_item] == '')
 
-      if !params[:sown_items].nil?
-        for sown_item in params[:sown_items]
-          which, id = sown_item.split('-')
-          case which
-          when 'sown_item'
-            target.sown_items << SownItem.find_by_id(id)
-          when 'to_sow_item'
-            item = !SownItem.find_by(item: ToSowItem.find_by_id(id).item).nil? ? SownItem.find_by(item: ToSowItem.find_by_id(id).item) : SownItem.create(item: ToSowItem.find_by_id(id).item)
-            target.sown_items << item
-          end
+    if !(params[:sown_items].nil? || (params[:sown_items].length == 1 && params[:sown_items][0] == ''))
+      for sown_item in params[:sown_items]
+        which, id = sown_item.split('-')
+        case which
+        when 'sown_item'
+          target.sown_items << SownItem.find_by(user_id: user.id, id: id)
+        when 'to_sow_item'
+          item = !SownItem.find_by(user_id: user.id, item: ToSowItem.find_by(user_id: user.id, id: id).item).nil? ? SownItem.find_by(user_id: user.id, item: ToSowItem.find_by(user_id: user.id, id: id).item) : user.sown_items.build(item: ToSowItem.find_by(user_id: user.id, id: id).item)
+          target.sown_items << item
         end
       end
-
-      if !params[:to_sow_items].nil?
-        for to_sow_item in params[:to_sow_items]
-          which, id = to_sow_item.split('-')
-          case which
-          when 'sown_item'
-            item = !ToSowItem.find_by(item: SownItem.find_by_id(id).item).nil? ? ToSowItem.find_by(item: SownItem.find_by_id(id).item) : ToSowItem.create(item: SownItem.find_by_id(id).item)
-            target.to_sow_items << item
-          when 'to_sow_item'
-            target.to_sow_items << ToSowItem.find_by_id(id)
-          end
-        end
-      end
-
-      target.save
-
-      redirect "/targets/#{target.id}"
     end
+
+    if !(params[:to_sow_items].nil? || (params[:to_sow_items].length == 1 && params[:to_sow_items][0] == ''))
+      for to_sow_item in params[:to_sow_items]
+        which, id = to_sow_item.split('-')
+        case which
+        when 'sown_item'
+          item = !ToSowItem.find_by(user_id: user.id, item: SownItem.find_by(user_id: user.id, id: id).item).nil? ? ToSowItem.find_by(user_id: user.id, item: SownItem.find_by(user_id: user.id, id: id).item) : user.to_sow_items.build(item: SownItem.find_by(user_id: user.id, id: id).item)
+          target.to_sow_items << item
+        when 'to_sow_item'
+          target.to_sow_items << ToSowItem.find_by(user_id: user.id, id: id)
+        end
+      end
+    end
+
+    target.save
+
+    redirect "/targets/#{target.id}"
   end
 
   # delete
