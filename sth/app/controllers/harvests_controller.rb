75ba574d4f7152dab171f0abd6aabcdf4332e72f
StevenDRiggs <sfriggs21@gmail.com>
2020-05-24 02:04:09 -0600

    refactored harvests/edit for private user data

--- sth/app/controllers/harvests_controller.rb
+++ sth/app/controllers/harvests_controller.rb
@@ -28,7 +28,7 @@ class HarvestsController < ApplicationController
     harvest.completed = params[:completed] == 'on'
     harvest.time_to_completion = "#{params[:months_to_completion]} months, #{params[:weeks_to_completion]} weeks, #{params[:days_to_completion]} days, #{params[:hours_to_completion]} hours, #{params[:minutes_to_completion]} minutes"
 
-    if !(params[:sources].nil? || (params[:sources].length == 1 && params[:sources] == ''))
+    if !(params[:sources].nil? || (params[:sources].length == 1 && params[:sources][0] == ''))
       for source_id in params[:sources]
         harvest.sources << Source.find_by(user_id: user.id, id: source_id)
       end
@@ -59,21 +59,22 @@ class HarvestsController < ApplicationController
       redirect '/'
     end
 
-    @harvest = Harvest.find_by_id(params[:id])
-    @sources = Source.all
+    @user = User.find_by_id(session[:rd])
+    @harvest = Harvest.find_by(user_id: @user.id, id: params[:id])
 
     erb :'/harvests/edit.html'
   end
 
   patch '/harvests/:id' do
-    harvest = Harvest.find_by_id(params[:id])
+    user = User.find_by_id(session[:rd])
+    harvest = Harvest.find_by(user_id: user.id, id: params[:id])
     harvest.completed = params[:completed] == 'on'
     harvest.time_to_completion = "#{params[:months_to_completion].to_i} months, #{params[:weeks_to_completion].to_i} weeks, #{params[:days_to_completion].to_i} days, #{params[:hours_to_completion].to_i} hours, #{params[:minutes_to_completion].to_i} minutes"
 
     harvest.sources = []
     if !(params[:sources].nil? || (params[:sources].length == 1 && params[:sources][0] == ''))
       for source_id in params[:sources]
-        source = Source.find_by_id(source_id)
+        source = Source.find_by(user_id: user.id, id: source_id)
         harvest.sources << source if !source.nil?
       end
     end
