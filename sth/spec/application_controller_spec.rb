16a20dff2cf657feb082dc9c446993514144777b
StevenDRiggs <sfriggs21@gmail.com>
2020-05-27 21:46:10 -0600

    first 2 application controller tests pass

--- sth/spec/application_controller_spec.rb
+++ sth/spec/application_controller_spec.rb
@@ -8,6 +8,13 @@ describe ApplicationController do
   it "responds with a welcome message" do
     get '/'
     expect(last_response.status).to eq(200)
-    expect(last_response.body).to include("Welcome to the Sinatra Template!")
+    expect(last_response.body).to include("Welcome to the Seed, Time and Harvest Database!")
+  end
+
+  it "requires login to view index" do
+    get '/index'
+    expect(last_response).to be_redirect
+    follow_redirect!
+    expect(last_response.body).to include("You must be logged in to view stored data")
   end
 end
