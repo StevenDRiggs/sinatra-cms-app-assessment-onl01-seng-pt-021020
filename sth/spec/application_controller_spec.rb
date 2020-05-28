bc31c113eb5babe5875f4788af97c67da9b62104
StevenDRiggs <sfriggs21@gmail.com>
2020-05-27 21:58:54 -0600

    ApplicationController root context passes

--- sth/spec/application_controller_spec.rb
+++ sth/spec/application_controller_spec.rb
@@ -5,16 +5,33 @@ def app
 end
 
 describe ApplicationController do
-  it "responds with a welcome message" do
-    get '/'
-    expect(last_response.status).to eq(200)
-    expect(last_response.body).to include("Welcome to the Seed, Time and Harvest Database!")
+  context "root page" do
+    it "responds with a welcome message" do
+      get '/'
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include("Welcome to the Seed, Time and Harvest Database!")
+    end
+
+    it "contains signup link" do
+      get '/'
+      expect(last_response.body).to include('<a href="/signup">Sign Up</a>')
+    end
+
+    it "contains login link" do
+      get '/'
+      expect(last_response.body).to include('<a href="/login">Log In</a>')
+    end
+
+    it "requires login to view index" do
+      get '/index'
+      expect(last_response).to be_redirect
+      follow_redirect!
+      expect(last_response.body).to include("You must be logged in to view stored data")
+    end
   end
 
-  it "requires login to view index" do
-    get '/index'
-    expect(last_response).to be_redirect
-    follow_redirect!
-    expect(last_response.body).to include("You must be logged in to view stored data")
+  context "signup page" do
+    it "properly displays page" do
+    end
   end
 end
