4476c141f4cb2162025ea8957bf81bf13b63d282
StevenDRiggs <sfriggs21@gmail.com>
2020-05-28 17:06:17 -0600

    Revert "7 examples pass"
    This reverts commit dadf5c31ea3735f38bf82751fc22a1931e892023.

--- sth/spec/controllers/application_controller_spec.rb
+++ sth/spec/controllers/application_controller_spec.rb
@@ -3,13 +3,11 @@ require 'spec_helper'
 describe ApplicationController do
 
   describe "Homepage" do
-    <<-PASS
     it 'loads the homepage' do
       get '/'
       expect(last_response.status).to eq(200)
       expect(last_response.body).to include("Welcome to the Seed, Time and Harvest Database!")
     end
-    PASS
   end
 
   describe "Signup Page" do
@@ -18,7 +16,6 @@ describe ApplicationController do
       user.delete if user
     end
 
-    <<-PASS
     it 'loads the signup page' do
       get '/signup'
       expect(last_response.status).to eq(200)
@@ -33,8 +30,8 @@ describe ApplicationController do
       post '/signup', params
       expect(last_response.location).to include("/index")
     end
-    PASS
 
+    <<-OUT
     it 'does not let a user sign up without a username' do
       params = {
         :username => "",
@@ -64,6 +61,7 @@ describe ApplicationController do
       post '/signup', params
       expect(last_response.location).to include('/signup')
     end
+    OUT
 
     it 'does not let a logged in user view the signup page' do
       params = {
